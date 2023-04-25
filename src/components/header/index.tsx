7eb2a45ab26b2ce5056f008fa01866c97543a265
George Koloidi <Battleaxe@bk.ru>
2023-04-26 01:03:08 +0200

    fix: remove conflict

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,26 @@
 import './index.scss';
 
+import { HelloWorld } from '../HelloWorld';
+import { useTranslation } from 'react-i18next';
+import { useTheme } from '../../providers/ThemeProvider';
+import classNames from 'classnames';
+
 export const Header = () => {
-  return <header className="header">Header</header>;
+  const { i18n } = useTranslation();
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <header className="header">
+      <div className={classNames('App', theme)}>
+        <button
+          onClick={() => {
+            i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en');
+          }}
+        >
+          Switch lang
+        </button>
+        <button onClick={toggleTheme}> Switch theme </button>
+        <HelloWorld />
+      </div>
+    </header>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-<<<<<<< a756ea4548ae18ea62584d335e08b9df263a9142
-import { ThemeProvider } from 'app/providers/ThemeProvider';
-import { App } from './app/App';
+import { ThemeProvider } from './providers/ThemeProvider';
+import { App } from '../src/components/app';
 
 import './shared/config/i18n/i18n';
 
 import './app/styles/index.scss';
-=======
-import { App } from './components/app';
->>>>>>> feat: initial architecture
+
+// import { App } from './components/app';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-<<<<<<< a756ea4548ae18ea62584d335e08b9df263a9142
       <ThemeProvider>
         <App />
       </ThemeProvider>
-=======
-      <App />
->>>>>>> feat: initial architecture
     </BrowserRouter>
   </React.StrictMode>
 );
