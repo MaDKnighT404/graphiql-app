d8445ea43d8a6b3dd900cce5215f8cc4307c306f
George Koloidi <Battleaxe@bk.ru>
2023-04-26 12:44:06 +0200

    fix: path to App

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './providers/ThemeProvider';
-import { App } from '../src/components/app';
-
+import { App } from './components/app';
 import './shared/config/i18n/i18n';
-
-import './app/styles/index.scss';
-
-// import { App } from './components/app';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
