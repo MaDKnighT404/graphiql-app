26086b76023e2dad22a779e9e6726e9b8932bd42
George Koloidi <Battleaxe@bk.ru>
2023-05-01 21:06:26 +0200

    feat: add firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,83 @@
+import { initializeApp } from 'firebase/app';
+import {
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  // sendPasswordResetEmail,
+  signOut,
+} from 'firebase/auth';
+import { getFirestore, query, getDocs, collection, where, addDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC9gvKelvSQj-RPs-A0mEEh5SVDACTUVDc',
+  authDomain: 'fir-auth-graphql.firebaseapp.com',
+  projectId: 'fir-auth-graphql',
+  storageBucket: 'fir-auth-graphql.appspot.com',
+  messagingSenderId: '891381859787',
+  appId: '1:891381859787:web:214b69cab61c63c418a24e',
+  measurementId: 'G-840M8DGM55',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, 'users'), where('uid', '==', user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, 'users'), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: 'google',
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const logInWithEmailAndPassword = async (email: string, password: string) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const registerWithEmailAndPassword = async (name: string, email: string, password: string) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, 'users'), {
+      uid: user.uid,
+      name,
+      authProvider: 'local',
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  // sendPasswordReset,
+  logout,
+};
