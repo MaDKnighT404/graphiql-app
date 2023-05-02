0503f43f212a454e418ad582681640bbf863148d
George Koloidi <Battleaxe@bk.ru>
2023-05-02 15:53:12 +0200

    fix: remove redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
-import { Provider } from 'react-redux';
 import ErrorBoundary from 'app/providers/ErrorBoundary/ErrorBoundary';
 import { App } from './components/App';
-import store from './redux/store';
 import './shared/config/i18n/i18n';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <ErrorBoundary>
-          <ThemeProvider>
-            <App />
-          </ThemeProvider>
-        </ErrorBoundary>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
   </React.StrictMode>
 );
