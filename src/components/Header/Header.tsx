55a7b43f8972de94b1ffc33cef32a45f5a6521c0
George Koloidi <Battleaxe@bk.ru>
2023-05-04 18:19:42 +0200

    feat: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,14 @@
-import { useTranslation } from 'react-i18next';
-import { Button, ButtonSize, ButtonTheme } from 'components/Button/Button';
-import { auth } from 'firebase/firebase';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
 import { Logo } from 'components/Logo';
 import { Options } from 'components/Options';
+import { User } from 'components/User/User';
 import styles from './Header.module.scss';
 
 export const Header: React.FC = () => {
-  const navigate = useNavigate();
-  const { t } = useTranslation();
-  const [user] = useAuthState(auth);
-
-  const handleLogout = async () => {
-    await signOut(auth);
-    navigate('/');
-  };
-
   return (
     <header className={styles.header}>
       <Logo />
+      <User />
       <Options />
-
-      <Button onClick={handleLogout} size={ButtonSize.M} theme={ButtonTheme.OUTLINE}>
-        {t('Logout')}
-      </Button>
-      {user && <span>{user.displayName}</span>}
     </header>
   );
 };
