7576bde0ac163b5b8a0f052916942b47bd1018a7
George Koloidi <Battleaxe@bk.ru>
2023-05-04 20:36:18 +0200

    feat: add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,27 @@
-import { useEffect } from 'react';
+import rsSchoolLogo from '../../shared/assets/images/rs_school_logo.svg';
+import githubLogo from '../../shared/assets/images/github_logo.svg';
 import styles from './Footer.module.scss';
 
 export const Footer = () => {
-  // test errorboundary!
-  /*  useEffect(() => {
-    if (Math.random() > 0.5) throw new Error();
-  }, []); */
-  return <footer className={styles.footer}> 000</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.teamWrapper}>
+        <a href="https://github.com/egatsak">
+          <img src={githubLogo} className={styles.githubLogo} alt="Github logo" />
+        </a>
+        <a href="https://github.com/Exelery">
+          <img src={githubLogo} className={styles.githubLogo} alt="Github logo" />
+        </a>
+        <a href="https://github.com/MaDKnighT404">
+          <img src={githubLogo} className={styles.githubLogo} alt="Github logo" />
+        </a>
+      </div>
+      <p>2023</p>
+      <div className={styles.rsschoolWrapper}>
+        <a href="https://rs.school/">
+          <img src={rsSchoolLogo} className={styles.rsSchoolLogo} alt="RSSchool logo" />
+        </a>
+      </div>
+    </footer>
+  );
 };
