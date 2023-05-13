d77c35ae046249a9e7acd749c4646464055eb344
George Koloidi <Battleaxe@bk.ru>
2023-05-13 23:31:18 +0200

    fix: add display flex to main class on graphql route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,14 +2,16 @@ import { AppRouter } from 'app/providers/router';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Header } from './Header/Header';
 import { Footer } from './Footer/Footer';
+import { useLocation } from 'react-router-dom';
 
 export const App = () => {
   const { theme } = useTheme();
+  const pathname = useLocation().pathname;
 
   return (
     <div className={`app ${theme}`}>
       <Header />
-      <main className="main">
+      <main className={`main ${pathname === '/graphql' ? 'mainFlex' : ''}`}>
         <AppRouter />
       </main>
       <Footer />
--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,8 +1,3 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from 'firebase/firebase';
-import { Loader } from 'components/Loader/Loader';
 import { useTranslation } from 'react-i18next';
 import styles from './WelcomePage.module.scss';
 
