a4850ab018ab5f7264cac73e7481a6fa0f3538d3
George Koloidi <Battleaxe@bk.ru>
2023-06-12 14:44:42 +0200

    doc: add link to deploy

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -8,16 +8,16 @@ interface UserProps {
 }
 
 export const User: React.FC<UserProps> = ({ scrollTop }) => {
-  const userName = useAuthState(auth)[0]?.displayName as string;
+  const userName = useAuthState(auth);
   const { t } = useTranslation();
-
+  console.log(userName)
   return (
     <>
       {userName && (
         <div className={`${styles.userWrapper} ${scrollTop && styles.userWrapperAnimated}`}>
           <h5 className={styles.userTitle}>{t('Hello')}</h5>
           <div className={styles.user}>
-            <span className={styles.userName}>{userName}</span>
+
           </div>
         </div>
       )}
